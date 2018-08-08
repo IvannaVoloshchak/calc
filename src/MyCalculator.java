7c67c8eb6fe655801773d4e17a7c9aaf928cc59f
Ivanna Voloshchak <zvir44@gmail.com>
2018-08-08 19:37:01 +0300

    changed number1 and number2 methods

--- src/MyCalculator.java
+++ src/MyCalculator.java
@@ -6,7 +6,7 @@ public class MyCalculator {
         double number1;
         String operation;
         double result = 0;
-        double memoryNum=0;
+        double memoryNum = 0;
 
         do {
             Scanner scanner = new Scanner(System.in);
@@ -14,7 +14,7 @@ public class MyCalculator {
 
             switch (operation) {
                 case "+":
-                   result= add();
+                    result = add();
                     break;
                 case "-":
                     result = subtract();
@@ -25,29 +25,29 @@ public class MyCalculator {
                 case "/":
                     result = divide();
                     break;
-                case"m":
-                  memoryNum= getMemory(result);
+                case "m":
+                    memoryNum = getMemory(result);
                     break;
                 case "m+":
-                   result= memoryPlus(memoryNum);
+                    result = memoryPlus(memoryNum);
                     break;
-                case"m-":
-                   result= memorySubtract(memoryNum);
+                case "m-":
+                    result = memorySubtract(memoryNum);
                     break;
                 case "mc":
-                   result= setMemory(memoryNum);// work not so as I aspect
+                    result = setMemory(memoryNum);// work not so as I aspect
                     break;
-               case "s-":
+                case "s-":
                     result = sameMinus(result);
                     break;
                 case "s+":
-                 result =   samePlus(result);
+                    result = samePlus(result);
                     break;
                 default:
                     System.out.println("Error");
                     break;
             }
-            printOperation( result);
+            printOperation(result);
         } while (true);
     }
 
@@ -60,14 +60,15 @@ public class MyCalculator {
         }
         return number;
     }
-    static double number1(){
-        double number1 = Double.parseDouble(getNumber());
-                return  number1;
+
+    static double number1() {
+        return Double.parseDouble(getNumber());
     }
+
     static double number2() {
-        double number2 = Double.parseDouble(getNumber());
-        return number2;
+       return Double.parseDouble(getNumber());
     }
+
     static String getOperation(Scanner scanner) {
         System.out.println("Choose operation:");
         String operations = scanner.next();
@@ -76,52 +77,56 @@ public class MyCalculator {
         }
         return operations;
     }
-    static double add(){
-        return  number1() + number2();
+
+    static double add() {
+        return number1() + number2();
 
     }
 
-    static double subtract(){
-        return number1()-number2();
+    static double subtract() {
+        return number1() - number2();
     }
 
     static double multiply() {
-        return   number1()*number2();
+        return number1() * number2();
     }
 
     static double divide() {
         if (number2() == 0) {
             System.out.println("Sorry, you cant divide by 0");
         }
-        return number1()/number2();
+        return number1() / number2();
     }
 
-    static void printOperation( double result) {
+    static void printOperation(double result) {
         System.out.println("The current value is  = " + result);
     }
 
     static double getMemory(double result) {
         double memoryNum = result;
         return memoryNum;
-        }
-        static double memoryPlus(double memoryNum){
-            return number1()+memoryNum;
-        }
-    static double memorySubtract(double memoryNum){
-        return  number1()-getMemory(memoryNum);
     }
-    static double setMemory(double memoryNum){ //помилка все одно пам'ятає те число яке пішло в пам'ять
+
+    static double memoryPlus(double memoryNum) {
+        return number1() + memoryNum;
+    }
+
+    static double memorySubtract(double memoryNum) {
+        return number1() - getMemory(memoryNum);
+    }
+
+    static double setMemory(double memoryNum) { //помилка все одно пам'ятає те число яке пішло в пам'ять
         getMemory(memoryNum);
-        memoryNum=0.0;
+        memoryNum = 0.0;
         return memoryNum;
     }
 
     static double sameMinus(double result) {
-           return  number1() - result;
+        return number1() - result;
     }
 
     static double samePlus(double result) {
-            return  number1() + result;
+        return number1() + result;
     }
 
     static void showMenu() {
@@ -130,12 +135,12 @@ public class MyCalculator {
                 "\n - enter to subtract;" +
                 "\n * enter to multiply;" +
                 "\n / enter to divide;" +
-                "\n m if u want to work with current result in the future and put it in memory;"+
-                "\n m+ if u want to add number to memory value "+
-                "\n m- if u want to subtract number to memory value "+
-                "\n mc if u want to clear memory"+
-                "\n s+ if u want to add some other num to current value;"+
-                "\n s- if you want to subtract other number from current value;"+
+                "\n m if u want to work with current result in the future and put it in memory;" +
+                "\n m+ if u want to add number to memory value " +
+                "\n m- if u want to subtract number to memory value " +
+                "\n mc if u want to clear memory" +
+                "\n s+ if u want to add some other num to current value;" +
+                "\n s- if you want to subtract other number from current value;" +
                 "\n q pres it if you want leave leave the program");
     }
 }
